d105217dce3be88de20101335cd2dbd215193f1d
Diaa Ayman <abugamel53@gmail.com>
2022-08-30 09:21:23 +0200

    MODIFI

--- src/components/ImageModal.js
+++ src/components/ImageModal.js
@@ -44,33 +44,33 @@ function ImageModal(props) {
       <div className="fixed z-10 inset-0 overflow-y-auto">
         <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
           <div className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
-            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 h-[550px] flex flex-col">
+            <div className="bg-white h-[550px] flex flex-col">
               <div className="flex-1 grid place-items-center">
                 {imageURL ? (
                   <img
                     src={imageURL}
                     alt="posted Image"
-                    className="w-full mb-4 object-cover object-top h-96"
+                    className="w-full  object-cover object-top h-96"
                   />
                 ) : (
                   <span className="font-semibold text-lg">Loading...</span>
                 )}
               </div>
 
-              <div className="flex overflow-hidden w-full items-center space-x-2 bg-green-400 rounded-md">
+              <div className="flex overflow-hidden w-vw items-center space-x-2 bg-green-400 ">
                 <input
                   onChange={(e) => setEnteredCaption(e.target.value)}
                   placeholder="Enter a Caption!"
-                  className="flex-1 outline-none rounded-md p-2 bg-green-400 placeholder-white"
+                  className="flex-1 outline-none rounded-md p-4 bg-green-400 placeholder-white"
                 />
                 <button
-                  className="p-2 text-white font-medium  hover:bg-green-700"
+                  className="p-4 text-white font-medium  hover:bg-green-700 "
                   onClick={uploadImageHandler}
                 >
                   UPLOAD
                 </button>
                 <button
-                  className="text-red-600 p-2 text-sm"
+                  className="text-red-600 p-4 text-sm"
                   onClick={hideModalHandler}
                 >
                   Cancel
